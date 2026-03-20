a32ec70729f4f4ecf108375095ca32597148720f
Kaushal Bakrania <fellix@192.168.1.103>
2026-03-20 18:19:55 +0000

    fix: remove bin/obj, add gitignore, enable swagger in production

--- Program.cs
+++ Program.cs
@@ -74,15 +74,12 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "FinTrackAPI v1");
         c.RoutePrefix = string.Empty;
     });
-}
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
